b681d21a3f44d0d34c0dc1d1960af88954c989f5
Àëêåêñàíäð Âàñèëü÷óê <alexandrvsko@yandex.ru>
2023-06-30 19:12:02 +0300

    magic numbers fix. amount validators

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,6 +9,9 @@ class IsOwner(BasePermission):
     """Only author have access for update/delete."""
 
     def has_object_permission(
-        self, request: Request, view: Callable, obj: models.Model,
+        self,
+        request: Request,
+        view: Callable,
+        obj: models.Model,
     ) -> bool:
         return request.method in SAFE_METHODS or obj.author == request.user
